59eb29ec027b7e8ea91a3b9bb09cb4874065deb7
Chava <flores.salvador@gmail.com>
2019-01-11 13:22:55 -0600

    Poniendo variable de ambiente en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,12 +16,15 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     //DB en mlab.com user: cafe-user-mongodb pwd: cafeUser2019
-    const host = 'ds163698.mlab.com';
-    const port = '63698';
-    const dbName = 'cafe';
-    const user = 'cafe-user-mongodb';
-    const password = 'cafeUser2019';
+    //heroku config:set MONGO_URI="mongodb://cafe-user-mongodb:cafeUser2019@ds163698.mlab.com:63698/cafe"
 
-    urlDB = `mongodb://${user}:${password}@${host}:${port}/${dbName}`;
+    //const host = 'ds163698.mlab.com';
+    //const port = '63698';
+    //const dbName = 'cafe';
+    //const user = 'cafe-user-mongodb';
+    //const password = 'cafeUser2019';
+
+    //urlDB = `mongodb://${user}:${password}@${host}:${port}/${dbName}`;
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URL_DB = urlDB;
